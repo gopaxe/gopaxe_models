1cb9d185d28a1928a6658c3e76a32feb9900dd1b
yayahc <hamedcuenca5@gmail.com>
2024-01-03 13:42:02 +0000

    Added new model

--- lib/gopaxe_models.dart
+++ lib/gopaxe_models.dart
@@ -3,26 +3,26 @@
 // remenber to format this file, you can use: dart format
 // publish your package hint: dart pub publish --dry-run
 // if you want to update your packages on power: dart pub upgrade --major-versions
-export 'package:gopaxe_models/models/worker/worker.dart';
-export 'package:gopaxe_models/models/admin/admin.dart';
-export 'package:gopaxe_models/models/pays/pays.dart';
+export 'package:gopaxe_models/models/domain/domain.dart';
 export 'package:gopaxe_models/models/sign_in/sign_in_worker.dart';
 export 'package:gopaxe_models/models/notification/notification.dart';
-export 'package:gopaxe_models/models/image_uploaded/image_uploaded.dart';
-export 'package:gopaxe_models/models/competence/competence.dart';
-export 'package:gopaxe_models/models/ville/ville.dart';
-export 'package:gopaxe_models/models/auth/auth.dart';
-export 'package:gopaxe_models/models/domain/domain.dart';
-export 'package:gopaxe_models/models/type_compte/type_compte.dart';
-export 'package:gopaxe_models/models/metier/metier.dart';
-export 'package:gopaxe_models/models/client/client.dart';
-export 'package:gopaxe_models/models/service/service.dart';
+export 'package:gopaxe_models/models/profile_base/profile_base.dart';
+export 'package:gopaxe_models/models/admin/admin.dart';
+export 'package:gopaxe_models/models/transaction/transaction.dart';
+export 'package:gopaxe_models/models/subscription/subscription.dart';
 export 'package:gopaxe_models/models/jwt_role/rhumaine_output.dart';
 export 'package:gopaxe_models/models/jwt_role/jwt_response.dart';
 export 'package:gopaxe_models/models/jwt_role/role.dart';
 export 'package:gopaxe_models/models/device_info/device_info.dart';
-export 'package:gopaxe_models/models/transaction/transaction.dart';
-export 'package:gopaxe_models/models/profile_base/profile_base.dart';
-export 'package:gopaxe_models/models/subscription/subscription.dart';
-export 'package:gopaxe_models/models/http_error/http_error.dart';
+export 'package:gopaxe_models/models/pays/pays.dart';
+export 'package:gopaxe_models/models/auth/auth.dart';
+export 'package:gopaxe_models/models/service/service.dart';
+export 'package:gopaxe_models/models/metier/metier.dart';
+export 'package:gopaxe_models/models/worker/worker.dart';
+export 'package:gopaxe_models/models/ville/ville.dart';
+export 'package:gopaxe_models/models/competence/competence.dart';
+export 'package:gopaxe_models/models/image_uploaded/image_uploaded.dart';
+export 'package:gopaxe_models/models/type_compte/type_compte.dart';
+export 'package:gopaxe_models/models/client/client.dart';
 export 'package:gopaxe_models/models/comment/comment.dart';
+export 'package:gopaxe_models/models/http_error/http_error.dart';
